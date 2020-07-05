354d3ff2ae173c32fa34a50b0abfaa8ffa1729df
Labinator Team <info@labinator.com>
2020-07-05 17:15:48 +0800

    Fixes a bug + code formatting.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,109 +1,84 @@
 import React, { Component } from "react";
 import "./Home.css";
-
 class Home extends Component {
-  state = {
-    //searched: "",
-	val: "",
-	selectVal: "",
-	emptyInput: 0,
-	usaInput: 0,
-	drugDetails: "",
-	noResult: 0,
-  };
-
-  handleChange = (e) => {
-    return this.setState({ val: e.target.value, drugDetails: "" });
-  };
-
-  handleSelectChange = (e) => {
-    return this.setState({ selectVal: e.target.value, drugDetails: "", val: "" });
-  };
-
-  keyDownEvent = (e) => {
-
-    if(e.key === "Enter") { //13 is the enter keycode
-      if (this.state.val === "") {
-	  this.setState({emptyInput: 1});
-	  setTimeout(() => {this.setState({emptyInput: 0})}, 1000);
-	  return;
-	}
-
-	//this.setState({ val: "", selectVal: "" });
-	let searchedVal = this.state.val.toLowerCase();
-	let apilink = `https://api.fda.gov/drug/ndc.json?search=${this.state.selectVal}:%22${searchedVal}%22&limit=1`;
-
-	fetch(apilink)
-      .then(res => res.json())
-      .then(
-        (result) => {
-		  if (typeof result.error !== 'undefined') {
-		    this.setState({ noResult: 1 });
-			setTimeout(() => {this.setState({noResult: 0})}, 1000);
-		  }
-		  else {
-
-			this.setState({ drugDetails: result.results[0] });
-		  }
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+    state = {
+        //searched: "",
+        val: "",
+        selectVal: "",
+        emptyInput: 0,
+        usaInput: 0,
+        drugDetails: "",
+        noResult: 0,
+    };
+    handleChange = (e) => {
+        return this.setState({ val: e.target.value, drugDetails: "" });
+    };
+    handleSelectChange = (e) => {
+        return this.setState({ selectVal: e.target.value, drugDetails: "", val: "" });
+    };
+    keyDownEvent = (e) => {
+        if (e.key === "Enter") { //13 is the enter keycode
+            if (this.state.val === "") {
+                this.setState({ emptyInput: 1 });
+                setTimeout(() => { this.setState({ emptyInput: 0 }) }, 1000);
+                return;
+            }
+            //this.setState({ val: "", selectVal: "" });
+            let searchedVal = this.state.val.toLowerCase();
+            let apilink = `https://api.fda.gov/drug/ndc.json?search=${this.state.selectVal}:%22${searchedVal}%22&limit=1`;
+            fetch(apilink).then(res => res.json()).then(
+                (result) => {
+                    if (typeof result.error !== 'undefined') {
+                        this.setState({ noResult: 1 });
+                        setTimeout(() => { this.setState({ noResult: 0 }) }, 1000);
+                    } else {
+                        this.setState({ drugDetails: result.results[0] });
+                    }
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                })
         }
-      )
-
     }
-}
-
-  onSubmit = (e) => {
-
-	if (this.state.val === "") {
-	  this.setState({emptyInput: 1});
-	  setTimeout(() => {this.setState({emptyInput: 0})}, 1000);
-	  return;
-	}
-
-	//this.setState({ val: "", selectVal: "" });
-	let searchedVal = this.state.val.toLowerCase();
-	let apilink = `https://api.fda.gov/drug/ndc.json?search=${this.state.selectVal}:%22${searchedVal}%22&limit=1`;
-
-	fetch(apilink)
-      .then(res => res.json())
-      .then(
-        (result) => {
-		  if (typeof result.error !== 'undefined') {
-		    this.setState({ noResult: 1 });
-			setTimeout(() => {this.setState({noResult: 0})}, 1000);
-		  }
-		  else {
-
-			this.setState({ drugDetails: result.results[0] });
-		  }
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+    onSubmit = (e) => {
+        if (this.state.val === "") {
+            this.setState({ emptyInput: 1 });
+            setTimeout(() => { this.setState({ emptyInput: 0 }) }, 1000);
+            return;
         }
-      )
-  };
-
-  showContent = (e) => {
-	if (this.state.drugDetails === "") {
-	    return ( <p></p> );
-	}
-	else {
-		return (
-		  <div className="display-flex flex-column flex-wrap margin-2 margin-top-1 drugInfo">
+        //this.setState({ val: "", selectVal: "" });
+        let searchedVal = this.state.val.toLowerCase();
+        let apilink = `https://api.fda.gov/drug/ndc.json?search=${this.state.selectVal}:%22${searchedVal}%22&limit=1`;
+        fetch(apilink).then(res => res.json()).then(
+            (result) => {
+                if (typeof result.error !== 'undefined') {
+                    this.setState({ noResult: 1 });
+                    setTimeout(() => { this.setState({ noResult: 0 }) }, 1000);
+                } else {
+                    this.setState({ drugDetails: result.results[0] });
+                }
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            })
+    };
+    showContent = (e) => {
+        if (this.state.drugDetails === "") {
+            return (<p></p>);
+        } else {
+            return (<div className="display-flex flex-column flex-wrap margin-2 margin-top-1 drugInfo">
 	      <div className="display-flex flex-row flex-wrap">
 		  <p><span className="text-black text-bold">Product NDC: </span>{this.state.drugDetails.product_ndc}</p></div>
 		  <div className="display-flex flex-row flex-wrap">
@@ -128,17 +103,14 @@ class Home extends Component {
 		  <p><span className="text-black text-bold">Product Type: </span>{this.state.drugDetails.product_type}</p></div>
 		  <div className="display-flex flex-row flex-wrap">
 		  <p><span className="text-black text-bold">Product Id: </span>{this.state.drugDetails.product_id}</p></div>
-	      </div>
-		);
-	  }
-  }
-
-  onReset = (e) => {
-	return this.setState({ val: "", drugDetails: "", selectVal: "" });
-  };
-
-  render() {
-    return <React.Fragment>
+	      </div>);
+        }
+    }
+    onReset = (e) => {
+        return this.setState({ val: "", drugDetails: "", selectVal: "" });
+    };
+    render() {
+        return <React.Fragment>
 			 <div className="display-flex flex-column flex-wrap">
 	         <div className="display-flex flex-row padding-2 formDiv flex-wrap margin-right-4">
 
@@ -149,6 +121,7 @@ class Home extends Component {
 				<option value="brand_name">Proprietary Name</option>
 				<option value="generic_name">Nonproprietary Name</option>
 				<option value="active_ingredients.name">Active Ingredients Name</option>
+			   <select>
 			   </div>
 
 			 <div className="margin-top-0 margin-left-3 margin-bottom-2">
@@ -179,7 +152,6 @@ class Home extends Component {
 			</div>
 
 		  </React.Fragment>;
-  }
+    }
 }
-
 export default Home;
